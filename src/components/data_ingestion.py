97c607749d017cfcd9f3bae66007a6fa18bc50a1
AmmanSajid1 <ammansajid1@gmail.com>
2024-07-17 13:05:00 +0100

    add

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,8 +2,8 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-from src.components.data_transformation import DataTransformationConfig
-from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -49,4 +49,8 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(f"R2 Score for Best Model:{model_trainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr)}")
+    
